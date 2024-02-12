ed5373b503568e8b1f179f0c1c671ad4e15a2fff
DESKTOP-9VU9I7S\Rai <ajipunk008@gmail.com>
2024-02-12 13:27:51 +0700

    init tutorial

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { MembersProvider } from './stores/members.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MembersProvider>
-      <App />
-    </MembersProvider>
+    <App />
   </React.StrictMode>,
 )
